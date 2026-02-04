cdaa09c62993a50164953922c0d7df69d35a88ec
ruiznaomy13 <109822565+ruiznaomy13@users.noreply.github.com>
2026-02-04 23:56:48 +0100

    started move

--- common.h
+++ common.h
@@ -1,21 +1,25 @@
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<stdbool.h>
-#include	<assert.h>
-#include	<string.h>
+#ifndef COMMON_H
+# define COMMON_H
+# include	<stdio.h>
+# include	<stdlib.h>
+# include	<stdbool.h>
+# include	<assert.h>
+# include	<string.h>
 
-#define	NEW_GAME	1
-#define	SAVE_GAME	2
-#define	LOAD_GAME	3
-#define	RESUME_GAME	4
-#define	EXIT		5
+# define	NEW_GAME	1
+# define	SAVE_GAME	2
+# define	LOAD_GAME	3
+# define	RESUME_GAME	4
+# define	EXIT		5
 
-#define PLAYER	'A'
-#define WALL	'#'
-#define BOX		'B'
-#define GOAL	'G'
-#define P_GOAL	'Y'
-#define B_GOAL	'X'
-#define EMPTY	'.'
+# define PLAYER	'A'
+# define WALL	'# '
+# define BOX		'B'
+# define GOAL	'G'
+# define P_GOAL	'Y'
+# define B_GOAL	'X'
+# define EMPTY	'.'
 
-#define	MAX_STR_LENGTH	50
+# define	MAX_STR_LENGTH	50
+
+#endif
