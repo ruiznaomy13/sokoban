5edd1b0ae66a1d4aabb471363c438cb110582564
mmxhxmm <mahammalikk1211@gmail.com>
2026-02-19 13:44:21 +0100

    added the files back

---
+++ lab_2/session.c
@@ -0,0 +1,52 @@
+#include "session.h"
+/**** LAB 1 - functions to program (start here) ****/
+// using init_game and choose_level from session.h to restart the session
+void	restart_session_game(Session *session)
+{
+	free_game(&session->current_game);
+	init_game(&session->current_game);
+	choose_level(&session->current_game);
+}
+
+// This function is initialisng the session by setting the best score to 'zero'
+// And it also initialise the current game by using init_game function
+void	init_session(Session *session)
+{
+	// TODO
+	for (int i=0; i <= MAX_LEVELS; i++)
+		session->best_score[i] = 0;
+	init_game(&session->current_game);
+}
+
+// Prints the best score and the current game data
+void	print_session(Session *session)
+{ 
+	int	lvl = session->current_game.level;
+	printf("Best score for level %d: %d\n", lvl, session->best_score[lvl]);
+	printf("Current Level:%d, Current Score:%d\n", session->current_game.level, session->current_game.score);
+}
+// Ths function is assigning the current score to the best score
+void	new_game_score(Session *session)
+{
+	// Initialising two variables by current game score and lvl
+	int	score = session->current_game.score;
+	int	level = session->current_game.level;
+	if(score <= 0)
+		return ; // not solved yet
+	// If the best score is either zero or greater than the current score assign current score to best score
+	if (session->best_score[level] == 0 || session->best_score[level] > score)
+		session->best_score[level] = score;
+}
+/**** LAB 1 - functions to program (end here) ****/
+
+// LAB 2 - functions
+void free_session(Session *s)
+{
+	/*
+		checks if there is a session, if not returrns and don't do nothing
+		otherwise free the game memmory
+	*/
+	if (!s)
+		return ;
+	free_game(&s->current_game);
+}
---
+++ lab_2/utils.c
@@ -0,0 +1,33 @@
+#include "utils.h"
+// BORRAR
+int read_int()
+{
+  int res, val;
+  
+  do
+  {
+    res = scanf("%d", &val);
+  } while (res != 1);
+
+  return val;
+}
+
+void  read_filename(char *filename)
+{
+  int res;
+  
+  do
+  {
+	  res = scanf("%s", filename);
+  } while (res != 1);
+}
+
+int max(int a, int b)
+{
+  return (a > b ? a : b);
+}
+
+int min(int a, int b)
+{
+  return (a < b ? a : b);
+}
